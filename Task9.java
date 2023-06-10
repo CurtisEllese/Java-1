3808e39deb9a20db3959573960e0d9914de617ab
CurtisEllese <mama-karina@bk.ru>
2023-06-10 23:54:37 +0600

    Add new solution in Task 9

--- Task9.java
+++ Task9.java
@@ -6,46 +6,46 @@
 // C использованием только 1 массива
 
 
-// public class Task9 {
-//     public static void main(String[] args) {
-//         int[] nums = {3,2,4,3,1,5,3,3}; // [2.4.3. . . . .3.3.3]
-//         //            0,1,2,3,4,5,6,7.8.9       0,1,2,3,4,5,6,7.8.9
-//         int val = 3;
-//         for (int i = 1; i < nums.length; i++) { // i = 1 < 10
-//             if (nums[i - 1] == val) {
-//                 nums[i - 1] = nums[i];
-//                 for (int j = i; j < nums.length - 1; j++) {  // j = 1
-//                     nums[j] = nums[j + 1];
-//                 }
-//                 nums[nums.length - 1] = val;
-//             }
-//         }
-//         for (int i : nums) {
-//             System.out.println(i);
-//         }
-//     }
-// }
-
 public class Task9 {
     public static void main(String[] args) {
-        int[] nums = {3, 3, 3, 2, 4, 3, 1, 5, 3, 3};
+        int[] nums = {3, 3, 3, 2, 4, 3, 1, 5, 3, 3}; 
         int val = 3;
-        int lastIndex = nums.length - 1;
-
-        for (int i = 0; i < lastIndex;) {
-            if (nums[i] == val) {
-                for (int j = i; j < lastIndex; j++) {
+        for (int i = 1; i < nums.length - i; i++) {
+            if (nums[i - 1] == val) {
+                nums[i - 1] = nums[i];
+                for (int j = i; j < nums.length - 1; j++) { 
                     nums[j] = nums[j + 1];
                 }
-                nums[lastIndex] = val;
-                lastIndex--;
-            } else {
-                i++;
+                nums[nums.length - 1] = val;
+                i--;
             }
         }
-
         for (int i : nums) {
             System.out.println(i);
         }
     }
 }
+
+// public class Task9 {
+//     public static void main(String[] args) {
+//         int[] nums = {3, 3, 3, 2, 4, 3, 1, 5, 3, 3};
+//         int val = 3;
+//         int lastIndex = nums.length - 1;
+
+//         for (int i = 0; i < lastIndex;) {
+//             if (nums[i] == val) {
+//                 for (int j = i; j < lastIndex; j++) {
+//                     nums[j] = nums[j + 1];
+//                 }
+//                 nums[lastIndex] = val;
+//                 lastIndex--;
+//             } else {
+//                 i++;
+//             }
+//         }
+
+//         for (int i : nums) {
+//             System.out.println(i);
+//         }
+//     }
+// }
