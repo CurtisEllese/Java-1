c9d87adadfc2a74ea3505a2c13fa1582aab8e219
CurtisEllese <mama-karina@bk.ru>
2023-07-03 01:58:08 +0600

    Add Task 18

---
+++ Task18.java
@@ -0,0 +1,20 @@
+// Заполнить список десятью случайными числами, отсортировать его с помощью метода sort() и вывести на экран.
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Random;
+
+public class Task18 {
+    public static void main(String[] args) {
+        ArrayList<Integer> intList = new ArrayList<>();
+        Random rnd = new Random();
+
+        for (int i = 0; i < 10; i++) {
+            intList.add(rnd.nextInt(100));
+        }
+
+        intList.sort(null);
+
+        System.out.println(intList);
+    }
+}
